fd08df8a7aee2e277b944f72ce66d58d913669c6
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-07-15 16:42:13 -0700

    change the href

--- files/js/app/LoginTopMenu.jsx
+++ files/js/app/LoginTopMenu.jsx
@@ -7,6 +7,28 @@ class LoginTopMenu extends React.Component{
         alert("hi " + (this.props.name));
     }
 
+    componentDidMount() {
+        var me = this;
+
+        ReactDOM.TAO.dataEngine({
+            type: 'POST',
+            url: '/api/Tao/API_PG.api?code=PGdatabases',
+            data: {},
+            dataType: 'JSON',
+            timeout: (6 * 1000),
+            success: function(resultData){
+                me.setState({list : resultData.data, _TM : new Date().getTime()});
+            },
+            error : function(err) {
+                console.log('err');
+            },
+            spinner : me
+        });
+    }
+    componentDidUpdate(prevProps, prevState) {
+        var me = this;
+    }
+
     render(){
         var me = this;
         return(
