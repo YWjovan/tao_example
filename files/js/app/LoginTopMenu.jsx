c95ecc9c42b5ede92f75df67beae14c65c7a16d6
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-07-15 16:53:58 -0700

    change the href

--- files/js/app/LoginTopMenu.jsx
+++ files/js/app/LoginTopMenu.jsx
@@ -26,17 +26,17 @@ class LoginTopMenu extends React.Component{
         var me = this;
     }
 
-    handleClick(){
-        alert("hi " + (this.props.name));
+    handleClick(item){
+        alert("hi " + (item.name));
     }
 
     render(){
         var me = this;
         return(
             <div>
-                <span name = "Logo"><a href = "javascript:void(0)" onClick = {this.handleClick(me)}>Logo</a></span>
-                <span name = "Solution"><a href = "javascript:void(0)" onClick = {this.handleClick(me)}>Tao Solution</a></span>
-                <span name = "Sign"><a href = "javascript:void(0)" onClick = {this.handleClick(me)}>Sign Up</a></span>
+                <span name = "Logo"><a href = "javascript:void(0)">Logo</a></span>
+                <span name = "Solution"><a href = "javascript:void(0)">Tao Solution</a></span>
+                <span name = "Sign"><a href = "javascript:void(0)">Sign Up</a></span>
             </div>);
     }
 }
