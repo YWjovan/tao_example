556e342e94d455bab4542cd726bbb2f6f2c46133
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-07-15 16:49:44 -0700

    change the href

--- files/js/app/TAOindex.jsx
+++ files/js/app/TAOindex.jsx
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	// ReactDOM.TAO.load('topMenu', <LoginTopMenu param={{}} />, document.getElementById('topMenu'));
-	ReactDOM.TAO.load('topMenu', <TopMenu param={{}} />, document.getElementById('topMenu'));
+	ReactDOM.TAO.load('topMenu', <LoginTopMenu param={{}} />, document.getElementById('topMenu'));
+	// ReactDOM.TAO.load('topMenu', <TopMenu param={{}} />, document.getElementById('topMenu'));
 	ReactDOM.TAO.load('leftBox', <LeftBox param={{}} />, document.getElementById('leftBox'));
 	ReactDOM.TAO.load('rightBox', <TaoContent param={{}} />, document.getElementById('rightBox'));
 });
