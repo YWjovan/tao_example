5372688229442748a97cc703293603f339854576
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-07-18 16:58:40 -0700

    change the href

--- files/js/app/LoginTopMenu.jsx
+++ files/js/app/LoginTopMenu.jsx
@@ -1,39 +1,31 @@
 class LoginTopMenu extends React.Component{
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {list: []};
     }
 
     componentDidMount() {
         var me = this;
+
+        ReactDOM.TAO.dataEngine({
+            type: 'POST',
+            url: '/api/Tao/API_PG.api?code=PGdatabases',
+            data: {},
+            dataType: 'JSON',
+            timeout: (6 * 1000),
+            success: function(resultData){
+                me.setState({list : resultData.data, _TM : new Date().getTime()});
+            },
+            error : function(err) {
+                console.log('err');
+            },
+            spinner : me
+        });
     }
 
     componentDidUpdate(prevProps, prevState) {
         var me = this;
     }
-
-    // componentDidMount() {
-    //     var me = this;
-    //
-    //     ReactDOM.TAO.dataEngine({
-    //         type: 'POST',
-    //         url: '/api/Tao/API_PG.api?code=PGdatabases',
-    //         data: {},
-    //         dataType: 'JSON',
-    //         timeout: (6 * 1000),
-    //         success: function(resultData){
-    //             me.setState({list : resultData.data, _TM : new Date().getTime()});
-    //         },
-    //         error : function(err) {
-    //             console.log('err');
-    //         },
-    //         spinner : me
-    //     });
-    // }
-
-    // componentDidUpdate(prevProps, prevState) {
-    //     var me = this;
-    // }
 
     logoClick = () => {
 
