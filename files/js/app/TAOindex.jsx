2896ebe25e0c86e9b16db67490c15fa71eced616
Yue Wang <yw@yues-mbp.attlocal.net>
2019-08-20 16:55:21 -0700

    edit some pages

--- files/js/app/TAOindex.jsx
+++ files/js/app/TAOindex.jsx
@@ -4,6 +4,6 @@ $(document).ready(function() {
 	// ReactDOM.TAO.load('topMenu', <TopMenu param={{}} />, document.getElementById('topMenu'));
 	ReactDOM.TAO.load('leftBox', <LeftBox param={{}} />, document.getElementById('leftBox'));
 	ReactDOM.TAO.load('rightBox', <TaoContent param={{}} />, document.getElementById('rightBox'));
-	ReactDOM.TAO.load('footer', <TAORoot param={{}} />, document.getElementById('footer'));
+	ReactDOM.TAO.load('footer', <TopMenu param={{}} />, document.getElementById('footer'));
 });
 
