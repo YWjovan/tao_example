99424ea645d9152bc5092583f4103d6c2221d913
Yue Wang <yw@yues-mbp.attlocal.net>
2019-08-20 17:30:22 -0700

    edit some pages

--- files/js/app/TAOindex.jsx
+++ files/js/app/TAOindex.jsx
@@ -2,9 +2,9 @@ $(document).ready(function() {
 	// id, Tag, elementId
 	ReactDOM.TAO.load('topMenu', <LoginTopMenu param={{}} />, document.getElementById('topMenu'));
 	// ReactDOM.TAO.load('topMenu', <TopMenu param={{}} />, document.getElementById('topMenu'));
-	//ReactDOM.TAO.load('leftBox', <LeftBox param={{}} />, document.getElementById('leftBox'));
-	//ReactDOM.TAO.load('rightBox', <TaoContent param={{}} />, document.getElementById('rightBox'));
-	ReactDOM.TAO.load('loginMenu', <LoginMenu param={{}} />, document.getElementById('loginMenu'));
+	// ReactDOM.TAO.load('leftBox', <LeftBox param={{}} />, document.getElementById('leftBox'));
+	// ReactDOM.TAO.load('rightBox', <TaoContent param={{}} />, document.getElementById('rightBox'));
+	//ReactDOM.TAO.load('loginMenu', <LoginMenu param={{}} />, document.getElementById('loginMenu'));
 	ReactDOM.TAO.load('ourMission', <OurMission param={{}} />, document.getElementById('ourMission'));
 	ReactDOM.TAO.load('footer', <Footer param={{}} />, document.getElementById('footer'));
 });
