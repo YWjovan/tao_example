5cf14a09775d9bb23f4d0bb9ddf752fb5578c4e9
Yue Wang <yw@yues-mbp.attlocal.net>
2019-08-22 17:28:35 -0700

    add a new page

--- files/js/app/FeatureItem.jsx
+++ files/js/app/FeatureItem.jsx
@@ -7,7 +7,7 @@ class FeatureItem extends React.Component{
         return (
             <div>
                 <h4>{this.props.name}</h4>
-                <p>{this.props.content}</p>
+                {/*<p>{this.props.content}</p>*/}
             </div>
         );
     }
