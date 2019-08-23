dacd4c72f5d620edd0c35f6794c3cc463ed2732f
Yue Wang <yw@yues-mbp.attlocal.net>
2019-08-22 18:04:48 -0700

    add a new page

--- files/js/app/FeatureList.jsx
+++ files/js/app/FeatureList.jsx
@@ -1,14 +1,14 @@
 class FeatureList extends React.Component{
     constructor(props){
         super(props);
-        this.state = {"yw": "123", "eric": "234", "oliver": "345"};
+        this.state = {mm: [{"yw": "123"}, {"eric": "234"}, {"oliver": "345"}]};
     }
 
     render() {
         return (
             <div>
-                {this.state.map(function (item, index) {
-                    return (<FeatureItem  name={item.key} tag={item.value} />);
+                {this.state.mm.map(function (item, index) {
+                    return (<FeatureItem  name={item.key} tag={item.value} />)
                 })}
                 {/*<FeatureItem name="yw" tag={this.state.}/>*/}
                 {/*<FeatureItem name="eric" />*/}
