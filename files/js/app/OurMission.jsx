5f8f6cee0c76984c205f851bc471185474667660
Yue Wang <yw@yues-mbp.attlocal.net>
2019-08-22 17:40:54 -0700

    add a new page

--- files/js/app/OurMission.jsx
+++ files/js/app/OurMission.jsx
@@ -8,6 +8,7 @@ class OurMission extends React.Component{
             <div>
                 <h1>Our Mission</h1>
                 <p>Our mission is to create an online ecosystem to help entrepreneurs and SMBs grow and go international. Entrepreneurs have very good ideas, but often lack the resources, talent and support necessary to build them. Every successful business need the right business partners, services, and resources. TaoBase aims to help you facilitate easy and meaningful connections between entrepreneurs, services and resources. TaoBase integrates international directory, online & offline high-quality product and service resources in order to create a lifeline that benefits early stage startups and SMBs, connects established businesses to qualified providers, and empowers all entrepreneurs and businesses to grow.</p>
+                <Footer />
             </div>
         );
     }
