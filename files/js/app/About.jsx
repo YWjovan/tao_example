27c463f59d0b9ec0106126f1e058df08b1714020
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-08-23 16:33:45 -0700

    edit new pages

--- files/js/app/About.jsx
+++ files/js/app/About.jsx
@@ -4,7 +4,7 @@ class About extends React.Component{
         this.state = [{
             contents: [
                 'We envision a future where commerce between the U.S. and China is seamless, value-driven, and based on trust. TaoBase brings to fruition these goals as a one-stop shop business solution platform for US and China small and medium technology-related companies, distributors, and manufacturers.',
-                'Taobase is an innovative commercial platform based on removing cross-border barriers to information flow. We make value by tapping talent, entrepreneurs and SMB demand, linking each as well as international counterparts in a seamless business environment. Built on an AI and block-chain powered platform, users can easily search and match resources, find service providers and secure transactions based on user search queries in a plug-and-play manner. Taobase connects people with resources. \n'
+                'Taobase is an innovative commercial platform based on removing cross-border barriers to information flow. We make value by tapping talent, entrepreneurs and SMB demand, linking each as well as international counterparts in a seamless business environment. Built on an AI and block-chain powered platform, users can easily search and match resources, find service providers and secure transactions based on user search queries in a plug-and-play manner. Taobase connects people with resources.'
             ]
         }];
     }
