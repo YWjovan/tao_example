d5edab682910193de35836eda721af6e6fd4831c
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-08-28 14:32:26 -0700

    edit professional services&filing in premiumService page in Chinese Version

--- files/js/app/OurMission.jsx
+++ files/js/app/OurMission.jsx
@@ -17,7 +17,7 @@ class OurMission extends React.Component{
     render() {
         return (
             <div>
-                <h3 style={this.state.hstyle}>Our Mission</h3>
+                <h3 style={this.state.hstyle}>Our Mission！</h3>
                 <p style={this.state.pstyle}>Our mission is to create an online ecosystem to help entrepreneurs and SMBs grow and go international. Entrepreneurs have very good ideas, but often lack the resources, talent and support necessary to build them. Every successful business need the right business partners, services, and resources. TaoBase aims to help you facilitate easy and meaningful connections between entrepreneurs, services and resources. TaoBase integrates international directory, online & offline high-quality product and service resources in order to create a lifeline that benefits early stage startups and SMBs, connects established businesses to qualified providers, and empowers all entrepreneurs and businesses to grow.</p>
             </div>
         );
