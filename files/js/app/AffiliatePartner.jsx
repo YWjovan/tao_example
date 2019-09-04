a5d8e7672912a57e9b2bfb567e07e112ce5c5cf7
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-09-04 15:19:48 -0700

    edit new page

--- files/js/app/AffiliatePartner.jsx
+++ files/js/app/AffiliatePartner.jsx
@@ -35,18 +35,18 @@ class AffiliatePartner extends React.Component{
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                this.setState({isLoaded: true}, function () {
-                    console.log(this.state.isLoaded);
-                    this.setState({peopleList: data}, function () {
-                        console.log(this.state.peopleList)
+                this.setState({peopleList: data}, function () {
+                    console.log(this.state.peopleList);
+                    this.setState({isLoaded: true}, function () {
+                        console.log(this.state.isLoaded)
                     });
                 })
             });
     }
 
     render() {
-        console.log("isloaded?" + this.state.isLoaded);
-        console.log("peopleList:" + this.state.peopleList);
+        //console.log("isloaded?" + this.state.isLoaded);
+        // console.log("peopleList:" + this.state.peopleList);
         if(this.state.isLoaded) {
             return (
                 this.props.peopleList.people.map((item, index) => {
