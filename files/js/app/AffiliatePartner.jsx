e697f38e0d9609ca8b2afb9cd449d11072d528a1
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-09-04 15:22:12 -0700

    edit new page

--- files/js/app/AffiliatePartner.jsx
+++ files/js/app/AffiliatePartner.jsx
@@ -34,7 +34,7 @@ class AffiliatePartner extends React.Component{
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                //console.log(data);
                 this.setState({peopleList: data}, function () {
                     console.log(this.state.peopleList);
                     this.setState({isLoaded: true}, function () {
