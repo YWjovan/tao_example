fa1a83c3670f84fc64cad10ef1e5d8cd753c27dc
Yue Wang <yw@yues-mbp.attlocal.net>
2019-09-05 13:50:38 -0700

    edit professional services&filing in premiumService page in Chinese Version

--- files/js/app/LoginMenu.jsx
+++ files/js/app/LoginMenu.jsx
@@ -1,6 +1,7 @@
 class LoginMenu extends React.Component{
     constructor(props){
         super(props);
+        this.state = { list: []};
     }
 
     componentDidMount() {
@@ -12,6 +13,7 @@ class LoginMenu extends React.Component{
             dataType: 'JSON',
             timeout: (6 * 1000),
             success: function(resultData){
+                console.log(resultData);
                 me.setState({list : resultData.data, _TM : new Date().getTime()});
             },
             error : function(err) {
