13cac3b3eae30d29357e857232c060b48106a38d
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-09-04 17:08:51 -0700

    edit new page

--- files/js/app/LoginMenu.jsx
+++ files/js/app/LoginMenu.jsx
@@ -6,7 +6,18 @@ class LoginMenu extends React.Component{
     render() {
         return (
             <div>
-                <h1>this is login part!</h1>
+                <p>Logo</p>
+                <p>connect with</p>
+                <MediaLogin />
+                <p>or</p>
+                <input type='text' name='user' defaultValue='Email' /><br/>
+                <input type='text' name='password' defaultValue='Password' /><br/>
+                <select>
+                    <option>remember me</option>
+                </select>
+                <span><a>forgot password?</a></span>
+                <button>Login</button>
+                <span>Not register?</span><span><a>Sign up</a></span><span>for free membership</span>
             </div>
         );
     }
