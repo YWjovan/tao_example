9c3bc6501c45f2fe15577a81494ec2c0ebd1e481
Yue Wang <yw@yues-mbp.attlocal.net>
2019-09-07 17:26:32 -0700

    edit testLogin api

--- api/testLogin.js
+++ api/testLogin.js
@@ -3,12 +3,12 @@ var pg = TAO.require(TAO.env.root_path + '/vendor/pg/node_modules/pg');
 
 delete TAO.require.cache[TAO.env.config_path + '/dbSetting.json'];
 var setting =  TAO.require(TAO.env.config_path + '/dbSetting.json');
-var connStr = "postgres://evergreen:Ahiuhwqceiuw168@mydbinstance1.clphgmho1uem.us-west-1.rds.amazonaws.com:5432/ebiztiedb";
+// var connStr = "postgres://evergreen:Ahiuhwqceiuw168@mydbinstance1.clphgmho1uem.us-west-1.rds.amazonaws.com:5432/ebiztiedb";
 
 setting.dev.PG_evergreen.connectionTimeoutMillis = 6000;
 
-var client = new pg.Client(connStr);
-// var client = new pg.Client(setting.dev.PG_evergreen);
+// var client = new pg.Client(connStr);
+var client = new pg.Client(setting.dev.PG_evergreen);
 
 client.connect(function(err) {
     if(err) {
@@ -18,7 +18,7 @@ client.connect(function(err) {
         return;
     }
 
-    let str = 'SELECT * FROM blogs;';
+    let str = 'SELECT * FROM eliteuser_basic;';
     client.query(str, function(err, res) {
         if(err) {
             console.log('error message is ' + err.message);
