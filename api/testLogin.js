3dc4169afaa3a4d062ff509639a34c48e275204b
Yue Wang <yw@yues-mbp.attlocal.net>
2019-09-07 17:09:41 -0700

    edit testLogin api

--- api/testLogin.js
+++ api/testLogin.js
@@ -18,7 +18,7 @@ client.connect(function(err) {
         return;
     }
 
-    let str = 'SELECT * FROM eliteuser_basic WHERE email = yuewangjovan@gmail.com;';
+    let str = 'SELECT * FROM eliteuser_basic;';
     client.query(str, function(err, res) {
         if(err) {
             console.log('error message is ' + err.message);
