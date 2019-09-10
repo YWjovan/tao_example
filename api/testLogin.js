1d372d5ec70ebb7473e5b685bc47ce8425ff2ea9
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-09-10 13:30:54 -0700

    edit testLogin api

--- api/testLogin.js
+++ api/testLogin.js
@@ -34,7 +34,7 @@ client.connect(function(err) {
     client.query(sqlStr,
         function(err, result) {
             if(err) {
-                q_result.data  = [error.message];
+                q_result.data  = [err.message];
             } else {
                 q_result.data = result.rows;
             }
