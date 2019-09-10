adcc79f23cb96f9cfc9ffadc219597b033ac1977
Yue Wang <yw@Yues-MacBook-Pro.local>
2019-09-10 14:17:40 -0700

    edit testLogin page

--- api/testLogin.js
+++ api/testLogin.js
@@ -5,10 +5,10 @@ var pg = TAO.require(TAO.env.root_path + '/vendor/pg/node_modules/pg');
 // var setting =  TAO.require(TAO.env.config_path + '/dbSetting.json');
 var connStr = "postgres://evergreen:Ahiuhwqceiuw168@mydbinstance1.clphgmho1uem.us-west-1.rds.amazonaws.com:5432/ebiztiedb";
 
-setting.dev.PG_evergreen.connectionTimeoutMillis = 6000;
+// setting.dev.PG_evergreen.connectionTimeoutMillis = 6000;
 
-// var client = new pg.Client(connStr);
-var client = new pg.Client(setting.dev.PG_evergreen);
+var client = new pg.Client(connStr);
+// var client = new pg.Client(setting.dev.PG_evergreen);
 
 client.connect(function(err) {
     if(err) {
