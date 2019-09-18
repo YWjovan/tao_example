58b52cc7a2616957fec1983359bd2a53589ba358
yw <yuewangjovan@gmail.com>
2019-09-17 17:01:01 -0700

    test cookie

--- files/js/app/OurMission.jsx
+++ files/js/app/OurMission.jsx
@@ -12,13 +12,14 @@ class OurMission extends React.Component{
         //     pstyle: pstyle,
         //     hstyle: hstyle
         // };
+        this.state = { token: 'hello our_mission'};
     }
 
     render() {
         return (
             <div>
                 {/*<h3 style={this.state.hstyle}>Our Mission！</h3>*/}
-                <h3>Our Mission！</h3>
+                <h3>Our Mission</h3>
                 <p>Our mission is to create an online ecosystem to help entrepreneurs and SMBs grow and go international. Entrepreneurs have very good ideas, but often lack the resources, talent and support necessary to build them. Every successful business need the right business partners, services, and resources. TaoBase aims to help you facilitate easy and meaningful connections between entrepreneurs, services and resources. TaoBase integrates international directory, online & offline high-quality product and service resources in order to create a lifeline that benefits early stage startups and SMBs, connects established businesses to qualified providers, and empowers all entrepreneurs and businesses to grow.</p>
             </div>
         );
