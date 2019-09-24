8d8e2c75ec03eb6d722a871328ca0a6587c84ac8
yw <yuewangjovan@gmail.com>
2019-09-24 16:57:58 -0700

    test login

--- api/testLogin.js
+++ api/testLogin.js
@@ -1,6 +1,6 @@
 var user = TAO.req.body.user;
 var pass = TAO.req.body.pass;
-var passwordHash = require('PasswordHash');
+// var passwordHash = require('PasswordHash');
 
 delete TAO.require.cache[TAO.env.root_path + '/vendor/pg/node_modules/pg'];
 var pg = TAO.require(TAO.env.root_path + '/vendor/pg/node_modules/pg');
