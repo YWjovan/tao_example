f76400c3b42dde4ec7b7dff6f4726f6b11ca1957
yw <yuewangjovan@gmail.com>
2019-09-24 17:03:54 -0700

    test login

--- api/testLogin.js
+++ api/testLogin.js
@@ -34,7 +34,8 @@ client.connect(function(err) {
                     // if(passwordHash.verify(pass, q_data.data[0].passWordHash)) {
                     //     q_result.token = '123456789';
                     // }
-                    TAO.res.send(q_data);
+                    TAO.res.send(TAO.env.root_path);
+                    // TAO.res.send(q_data);
                     // TAO.res.send(q_result);
                 }, Math.floor(Math.random() * 3 + 3) * 500
             );
